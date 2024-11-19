6682c28dfca1bb6c51966496b643dc6172b6f74e
AbiralThapa100 <abiralthapa100@gmail.com>
2024-11-20 01:24:56 +0400

    Updated break line feature

--- Band_name_generator.py
+++ Band_name_generator.py
@@ -1,4 +1,4 @@
 print("Welcome to Brand Name Generator")
-city_name = input("Which city your grew up?")
-pet_name = input("What is your pet name")
+city_name = input("Which city your grew up?\n")
+pet_name = input("What is your pet Name?\n")
 print("Your Band Name is " + city_name + pet_name )
