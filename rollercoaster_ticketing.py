96aed591c506b2deb65b85843e7e91e83e7211ce
AbiralThapa100 <abiralthapa100@gmail.com>
2024-11-22 00:59:12 +0400

    nested if else statement added

--- rollercoaster_ticketing.py
+++ rollercoaster_ticketing.py
@@ -4,7 +4,13 @@ height= float(input("What is your height? "))
 age= int(input("What is your age? "))
 
 if height >= 100:
-    print("You can ride")
+
+    if age <= 7:
+        print("Your ticket is $5")
+    elif age <= 10:
+        print("Your ticket is $7")
+    else:
+        print("Your ticket is $10")
 
 else:
     print("Sorry, you can't ride")
