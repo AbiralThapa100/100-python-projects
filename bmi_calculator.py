c41cba4fb8829c49461020c22c648d82ecff519a
AbiralThapa100 <abiralthapa100@gmail.com>
2024-11-22 00:37:36 +0400

    bmi calculator added

---
+++ bmi_calculator.py
@@ -0,0 +1,19 @@
+print("Wecome to BMI Calculator")
+weight = int(input("What is your weight? "))
+height = float(input("What is yoyr height? "))
+bmi = weight /(height ** 2)
+
+if bmi <= 18:
+    print("You are Skinny")
+
+elif bmi <= 25:
+    print("You are Healthy")
+
+else:
+    print("You are too Fatty")
+
+
+
+
+
+
