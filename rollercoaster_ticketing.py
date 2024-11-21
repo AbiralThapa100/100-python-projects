e550d7a2d02f74da1137aa2324af4c78869401dc
AbiralThapa100 <abiralthapa100@gmail.com>
2024-11-22 01:18:44 +0400

    New feature for photo ticketing is added

--- rollercoaster_ticketing.py
+++ rollercoaster_ticketing.py
@@ -1,16 +1,25 @@
 print("Welcome to Rollercoaster ticketing.")
 
-height= float(input("What is your height? "))
+height= int(input("What is your height in cm? "))
 age= int(input("What is your age? "))
-
+bill = 0
 if height >= 100:
 
     if age <= 7:
+        bill = 5
         print("Your ticket is $5")
-    elif age <= 10:
+    elif age <= 20:
+        bill = 7
         print("Your ticket is $7")
     else:
+        bill = 10
         print("Your ticket is $10")
+
+    want_photo = input("Do you want a photo taken? Y or N.")
+    if want_photo == "Y":
+        bill += 3
+
+    print(f"Your final bill is {bill}")
 
 else:
     print("Sorry, you can't ride")
