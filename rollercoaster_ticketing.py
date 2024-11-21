3e3f9ffee80191bbb33e16622963225153b3f3ef
AbiralThapa100 <abiralthapa100@gmail.com>
2024-11-22 00:56:04 +0400

    New project added

---
+++ rollercoaster_ticketing.py
@@ -0,0 +1,11 @@
+print("Welcome to Rollercoaster ticketing.")
+
+height= float(input("What is your height? "))
+age= int(input("What is your age? "))
+
+if height >= 100:
+    print("You can ride")
+
+else:
+    print("Sorry, you can't ride")
+
