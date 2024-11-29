59a6cc8df48b13e1ee2ae143d55966bca1472f25
AbiralThapa100 <abiralthapa100@gmail.com>
2024-11-29 23:46:53 +0400

    new lesson

--- courses.py
+++ courses.py
@@ -80,4 +80,36 @@ is_winning = True
 
 print(f"Your score is = {score}, your height is {height}. You are winning is {is_winning}")
 
+#loop
+fruits = ["Apple", "Peach", "Pear"]
+for fruit in fruits:
+    print(fruit)
+    print(fruit + " pie")
+
+print(fruits)
+
+student_scores = [150, 142, 185, 120, 171, 184, 149, 24, 59, 68, 199, 78, 65, 89, 86, 55, 91, 64, 89]
+
+max_score = 0
+for score in student_scores:
+    if score > max_score:
+        max_score = score
+
+print(max_score)
+
+
+print(range(1, 10))  # Doesn't do anything
+
+for number in range(1, 10):  # Prints 1 to 9
+    print(number)
+
+for number in range(1, 11):  # Prints 1 to 10
+    print(number)
+
+
+# Gauss challenge
+total = 0
+for number in range(1, 101):
+    total += number
+print(total)
 
