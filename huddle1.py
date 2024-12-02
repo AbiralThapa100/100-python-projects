5501594017896c07715cc96e770900e871c7bfc0
AbiralThapa100 <abiralthapa100@gmail.com>
2024-12-02 23:47:44 +0400

    huddle2

--- huddle1.py
+++ huddle1.py
@@ -20,3 +20,30 @@ def step():
 
 for jump in range(6):
     step()
+
+huddle
+2
+
+
+def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
+
+
+def step():
+    turn_left()
+    move()
+    turn_right()
+    move()
+    turn_right()
+    move()
+    turn_left()
+
+
+while not at_goal():
+    if wall_in_front():
+        step()
+
+    else:
+        move()
