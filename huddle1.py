341419a68003cd47153922033768ca80c292bea8
AbiralThapa100 <abiralthapa100@gmail.com>
2024-12-02 23:45:58 +0400

    huddle1

---
+++ huddle1.py
@@ -0,0 +1,22 @@
+#huddle1
+
+
+def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
+
+
+def step():
+    move()
+    turn_left()
+    move()
+    turn_right()
+    move()
+    turn_right()
+    move()
+    turn_()
+
+
+for jump in range(6):
+    step()
