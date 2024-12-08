d8449c8a796cbef1f89d44fa74604ddc7ec2aa3f
AbiralThapa100 <abiralthapa100@gmail.com>
2024-12-08 23:55:49 +0400

    nested list in nested dictionary

--- courses.py
+++ courses.py
@@ -323,3 +323,19 @@ capitals = {
 
 nested_list = ["A", "B", ["C", "D"]]
 # print(nested_list[2][1])
+
+
+
+# Nested dictionary in a dictionary
+travel_log = {
+  "France": {
+    "cities_visited": ["Paris", "Lille", "Dijon"],
+    "total_visits": 12
+   },
+  "Germany": {
+    "cities_visited": ["Berlin", "Hamburg", "Stuttgart"],
+    "total_visits": 5
+   },
+}
+
+print(travel_log["Germany"]["cities_visited"][2])
