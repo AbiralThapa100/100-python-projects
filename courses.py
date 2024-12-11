e7328fa57d4cdc424bcddc2fbc825874945f8bdf
AbiralThapa100 <abiralthapa100@gmail.com>
2024-12-11 23:02:53 +0400

    blind auction

--- courses.py
+++ courses.py
@@ -392,3 +392,19 @@ def life_in_weeks(age):
 
 
 life_in_weeks(12)
+
+
+#blind auction
+
+from art import logo
+print(logo)
+
+def find_highest_bidder(bidding_record):
+    highest_bid = 0
+    winner = ""
+    for bidder in bidding_record:
+        bid_amount = bidding_record[bidder]
+        if bid_amount > highest_bid:
+            highest_bid = bid_amount
+            winner = bidder
+    print(f"The winner is {winner} with a bid of ${highest_bid}")
